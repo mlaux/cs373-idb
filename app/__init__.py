190bafbe5e0a649406b14b29d3bb0b41f3d9adbd
Matt Laux <matthew.laux@gmail.com>
2016-07-02 16:18:38 -0500

    add other pages to nav, create blank html files for them

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,26 @@ app = Flask(__name__)
 def home_page():
     return render_template('home.html')
 
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/card_types")
+def card_types_page():
+    return render_template('card_types.html')
+
+@app.route("/cards")
+def cards_page():
+    return render_template('cards.html')
+
+@app.route("/subtypes")
+def subtypes_page():
+    return render_template('subtypes.html')
+
+@app.route("/families")
+def families_page():
+    return render_template('families.html')
+
 if __name__ == "__main__":
     app.run()
 
